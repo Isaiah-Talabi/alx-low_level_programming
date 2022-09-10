4e4be480ad907bc17ecb6cff26f185c36c36fbdd
Isaiah-Talabi <isaiahtalabi9@gmail.com>
2022-09-09 19:30:40 -0700

    study

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,7 +12,7 @@ int main(void)
 	int number_left;
 	int number_right;
 
-	for (number_left = 31; number_right <= 75; number_left++)
+	for (number_left = 33; number_right <= 75; number_left++)
 	{
 		for (number_right = number_left + 1 ; number_right <= 75; number_right++)
 		{
