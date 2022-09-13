bf0ddcb6430a56243ec7f71c2a3e7c1ca456e922
Isaiah-Talabi <isaiahtalabi9@gmail.com>
2022-09-13 07:05:55 -0700

    nestedloop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,12 @@
-#include "stdio.h"
-#include "stdlib.h"
+#include "main.h"
 
 /**
  * main - print _putchar
- * Return: Always 0 (Correct)
+ * Return: 0 (Success)
  */
 
 int main(void)
 {
-	printf("_putchar\n");
-		return (0);
+	_putchar("_putchar\n");
+	return (0);
 }
