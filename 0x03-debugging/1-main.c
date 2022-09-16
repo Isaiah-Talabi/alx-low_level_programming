f9ca0872d7f073e49678f1a15bfaf3b25f576f98
Isaiah-Talabi <isaiahtalabi9@gmail.com>
2022-09-16 13:56:07 -0700

    h

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,21 +6,12 @@
  */
 
 int main(void)
-
 {
-
 	int i;
-
-
 
 	printf("Infinite loop incoming :(\n");
 
-
-
 	i = 0;
-
-
-
 	/*
 	 *while (i < 10)
 	 *{
@@ -28,7 +19,6 @@ int main(void)
 	 *putchar(i);
 	 * }
 	 */
-
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
