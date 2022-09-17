4fc4c06644f0b976a23537738fcd5bd174e353e9
Isaiah-Talabi <isaiahtalabi9@gmail.com>
2022-09-16 17:51:12 -0700

    taya

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,34 +6,22 @@
   * followed by a new line
   * Return: Always 0 (Success)
   */
+
 int main(void)
 {
-	long int n;
-	long int max;
-	long int i;
+	unsigned int long n = 612852475143, a = (int) sqrt(n);
 
-	n = 612852475143;
-	max = -1;
-
-	while (n % 2 == 0)
+	while (1)
 	{
-		max = 2;
-		n /= 2;
-	}
 
-	for (i = 3; i <= sqrt(n); i = i + 2)
-	{
-		while (n % i == 0)
+		if (n % a == 0)
 		{
-			max = i;
-			n = n / i;
+			printf("%lu \n", n / a);
+			break;
 		}
+		a--;
+
 	}
-
-	if (n > 2)
-		max = n;
-
-	printf("%ld\n", max);
 
 	return (0);
 }
