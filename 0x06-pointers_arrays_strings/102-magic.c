a6899e6425202dbdccd122374ea9c0d8b4520209
Isaiah-Talabi <isaiahtalabi9@gmail.com>
2022-09-21 16:10:22 -0700

    arr

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,7 +2,7 @@
 
 /**
  * main - entry point
- * Return: 0
+ * Return: Always 0
  */
 int main(void)
 {
@@ -11,7 +11,6 @@ int main(void)
 	int *p;
 
 	a[2] = 1024;
-
 	p = &n;
 	/*
 	 * write your line of code here...
@@ -21,7 +20,7 @@ int main(void)
 	 * - only one statement
 	 * - you are not allowed to code anything else than this line of code
 	 */
-	*(p + 5) = 98;
+	p[5] = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
