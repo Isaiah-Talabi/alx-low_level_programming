86d88e4dc0327f7832f420bf12f5d91f47a62c1f
Isaiah-Talabi <isaiahtalabi9@gmail.com>
2022-09-29 00:32:59 +0100

    rcsv

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,7 +5,7 @@
  * Return: void
  */
 
-void _puts_recursion(char *s)
+int _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,7 +2,7 @@ int _putchar(char c);
 int _puts_recursion(char *s);
 int _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
-int int factorial(int n);
+int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
