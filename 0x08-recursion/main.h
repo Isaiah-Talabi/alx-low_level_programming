7762286c695ccb686a2cc3f38dc7a331e268ce7c
Isaiah-Talabi <isaiahtalabi9@gmail.com>
2022-09-29 02:13:28 +0100

    m

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,6 @@
 int _putchar(char c);
 int _puts_recursion(char *s);
-int _print_rev_recursion(char *s);
+void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
