f011b09b5dcc270d1390d60a99db02cc5fd66a56
Isaiah-Talabi <isaiahtalabi9@gmail.com>
2022-10-01 03:18:29 +0100

    lib

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - Short description, single line
+ * @c: contain value to be compared
+ * Return: 0 if c is not lowercase, 1 otherwise
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
