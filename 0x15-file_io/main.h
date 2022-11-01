ba7eb87433b089505a5f33a22487006ed2328e03
Isaiah-Talabi <isaiahtalabi9@gmail.com>
2022-11-01 20:55:36 +0100

    stdio

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,7 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-
+#include <elf.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
@@ -11,4 +13,4 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif /* MAIN_H */
+#endif
