57911de7e286183d02c5757a6885796a550e1133
Isaiah-Talabi <isaiahtalabi9@gmail.com>
2022-12-12 13:40:44 -0800

    err

---
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabets
+ * @c: character is to be checked
+ * Return: 1 if c is an alphabet, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
